ca38e2fee9dd61dc678441a042a8d161ac63338b
okn-yu <haruka.koishikawa@gmail.com>
2018-05-02 12:27:25 +0900

    updated.

--- 07/CodeWriter.py
+++ 07/CodeWriter.py
@@ -1,27 +1,82 @@
-class Code:
-    def __init__(self, parsedAsmLines, symbolDict):
-        self.binaryList = []
+INITIAL_SP_ADDRESS = 256
 
-        for parsedAsmDict in parsedAsmLines:
-            self._parse(parsedAsmDict, symbolDict)
 
-    def _parse(self, parsedAsmDict, symbolDict):
-        symbol = parsedAsmDict.get('symbol')
+class CodeWriter:
+    def __init__(self, filePath, parsedVMLines):
+        self.asmLines = []
+        self.SP = INITIAL_SP_ADDRESS
+
+        self._initMemory()
+
+        for parsedVMDict in parsedVMLines:
+            lines = self._parse(parsedVMDict)
+            self.asmLines.extend(lines)
+
+        self._writeCode(filePath, self.asmLines)
+
+    def _initMemory(self):
+        self._updateSP()
+
+    def _updateSP(self):
+        self.asmLines.append('@' + str(self.SP))
+        self.asmLines.append('D=A')
+        self.asmLines.append('@SP')
+        self.asmLines.append('M=D')
+
+    def _parse(self, parsedAsmDict):
+        command = parsedAsmDict.get('command')
         commandType = parsedAsmDict.get('commandType')
+        arg1 = parsedAsmDict.get('arg1')
+        arg2 = parsedAsmDict.get('arg2')
 
-        if commandType == 'L_COMMAND':
-            return
+        if commandType == 'C_PUSH':
+            return self._writePushPop(arg1, arg2)
 
-        if commandType == 'C_COMMAND':
-            binCode = '111' + self._comp(parsedAsmDict.get('comp')) + self._dest(parsedAsmDict.get('dest')) + self._jump(
-                parsedAsmDict.get('jump'))
+        if commandType == 'C_ARITHMETIC':
+            return self._writeArithmetic(command)
 
-        if commandType == 'A_COMMAND':
-            if symbol.isdigit():
-                address = int(symbol)
-            else:
-                address = symbolDict[symbol]
+    def _writePushPop(self, arg1, arg2):
+        asmLines = []
 
-            binCode = '0' + (bin(address))[2:].zfill(15)
+        if arg1 == 'constant':
+            asmLines.append('@' + arg2)
+            asmLines.append('D=A')
+            asmLines.append('@' + str(self.SP))
+            asmLines.append('M=D')
 
-        self.binaryList.append(binCode)
+            self.SP += 1
+            self._updateSP()
+
+        if arg1 == 'static':
+            asmLines.append('@' + self.className + '.' + arg2)
+            asmLines.append('D=' + arg2)
+            asmLines.append('M=D')
+
+        return asmLines
+
+    def _writeArithmetic(self, command):
+        asmLines = []
+
+        if command == 'add':
+            self.SP -= 1
+            self._updateSP()
+            asmLines.append('@' + str(self.SP))
+            asmLines.append('D=M')
+
+            self.SP -= 1
+            self._updateSP()
+            asmLines.append('@' + str(self.SP))
+            asmLines.append('M=D+M')
+
+            self.SP += 1
+            self._updateSP()
+
+        return asmLines
+
+    def _writeCode(self, filePath, asmLines):
+        outFilePath = filePath.split('.')[0] + '.' + 'hack'
+
+        with open(outFilePath, 'w') as f:
+            for line in asmLines:
+                if line:
+                    f.write(line + '\n')
---
+++ 07/Parser.py
@@ -0,0 +1,74 @@
+commandDict = {
+    'add': 'C_ARITHMETIC',
+    'sub': 'C_ARITHMETIC',
+    'neg': 'C_ARITHMETIC',
+    'eq': 'C_ARITHMETIC',
+    'gt': 'C_ARITHMETIC',
+    'lt': 'C_ARITHMETIC',
+    'and': 'C_ARITHMETIC',
+    'or': 'C_ARITHMETIC',
+    'not': 'C_ARITHMETIC',
+    'push': 'C_PUSH',
+    'pop': 'C_POP',
+    'label': 'C_LABEL',
+    'goto': 'C_GOTO',
+    'if-goto': 'C_IF',
+    'function': 'C_FUNCTION',
+    'call': 'C_CALL',
+    'return': 'C_RETURN'
+}
+
+
+class Parser:
+    def __init__(self, filePath):
+        self._commandsList = []
+        self._currentCommand = ''
+        self.parsedVMLines = []
+
+        with open(filePath, mode='r') as file:
+            linesList = file.readlines()
+            # print(linesList)
+
+        for line in linesList:
+            # print(line)
+            if (line[0:2] == '//') or (line == '\r\n') or (line == '\n'):
+                pass
+            else:
+                # line: command //comment
+                command = (line.split('//')[0])
+                self._commandsList.append(command.split())
+
+        while True:
+            if self._hasMoreCommands():
+                self._advance()
+                self._parser()
+            else:
+                break
+
+    def _parser(self):
+        parsedVMDict = {
+            'command':None,
+            'commandType': None,
+            'arg1': None,
+            'arg2': None
+        }
+
+        for index, commandElement in enumerate(self._currentCommand):
+            if index == 0:
+                parsedVMDict['command'] = commandElement
+                parsedVMDict['commandType'] = commandDict[commandElement]
+            if index == 1:
+                parsedVMDict['arg1'] = commandElement
+            if index == 2:
+                parsedVMDict['arg2'] = commandElement
+
+        self.parsedVMLines.append(parsedVMDict)
+
+    def _hasMoreCommands(self):
+        if len(self._commandsList) > 0:
+            return True
+        else:
+            return False
+
+    def _advance(self):
+        self._currentCommand = self._commandsList.pop(0)
