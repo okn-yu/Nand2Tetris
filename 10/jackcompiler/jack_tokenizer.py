73ab940da7f2164f2a99248df07ed48da42b2596
okn-yu <haruka.koishikawa@gmail.com>
2018-06-21 00:09:27 +0900

    updated.

--- 10/jackcompiler/jack_tokenizer.py
+++ 10/jackcompiler/jack_tokenizer.py
@@ -6,82 +6,142 @@ class jack_tokenizer:
 
         self._currentLine = None
         self._currentToken = None
-        self._tolenList = []
+        self._tokensList = []
+        self._XMLTokensList = ['<tokens>\n']
         self._tokenize(filePath)
 
-        # for lines in self.linesList:
-        #     if (lines[0:2] == '//') or (lines == '\r\n') or (lines == '\n'):
-        #         pass
-        #     else:
-        #         # line: command //comment
-        #         command = (lines.split('//')[0])
-        #         self._commandsList.append(command.split())
-        #
-        # while True:
-        #     if self._has_more_tokens():
-        #         self._advance()
-        #         self._parser()
-        #     else:
-        #         break
+        while True:
+            if self._has_more_tokens():
+                self._advance()
+                self._tokenType()
+            else:
+                break
+
+        self._writeXML(filePath)
 
     def _tokenize(self, filePath):
 
-        effectiveLines = self._read_file(filePath)
-        tokenLists = self._read_lines(effectiveLines)
+        rawLines = self._read_jack_file(filePath)
+        effectiveLines = self._read_jack_line(rawLines)
+        self._divide_2_tokens(effectiveLines)
 
-    def _read_file(self, filePath):
+    def _read_jack_file(self, filePath):
+
+        with open(filePath, mode='r') as file:
+            return file.readlines()
+
+    def _read_jack_line(self, rawLines):
 
         effectiveLines = []
 
-        with open(filePath, mode='r') as file:
-            rawLines = file.readlines()
-
         for line in rawLines:
-            if (line[0:2] == '//') or (line == '\r\n') or (line == '\n') or (line == '\t\n'):
-                pass
-            elif '//' in line:
-                code = (line.split('//')[0]) # line: code // comment
-                effectiveLines.append(code)
-            elif line[-1:] == '\n':
-                code = line[:-1]
-                effectiveLines.append(code)
+
+            if (line[0:2] == '//') or (line == '\r\n') or (line == '\n') or (line == '\t\n') or (line[0:2] == '/*'):
+                continue
+
+            if '//' in line:
+                line = (line.split('//')[0])  # line: code // comment
+
+            line = line.strip()
+
+            effectiveLines.append(line)
 
         return effectiveLines
 
-    def _read_lines(self, effectiveLines):
+    def _divide_2_tokens(self, effectiveLines):
 
-        print(effectiveLines)
         for line in effectiveLines:
-            print(line)
 
+            # divide by dot.
+            if '.' in line:
+                line = line.split('.')[0] + ' . ' + line.split('.')[1]
 
-    def _parser(self):
-        parsedVMDict = {
-            'fileName': None,
-            'command': None,
-            'commandType': None,
-            'arg1': None,
-            'arg2': None
-        }
+            if ',' in line:
+                line = line.split(',')[0] + ' , ' + line.split(',')[1]
 
-        for index, commandElement in enumerate(self._currentCommand):
-            if index == 0:
-                parsedVMDict['fileName'] = commandElement
-            if index == 1:
-                parsedVMDict['command'] = commandElement
-                parsedVMDict['commandType'] = commandDict[commandElement]
-            if index == 2:
-                parsedVMDict['arg1'] = commandElement
-            if index == 3:
-                parsedVMDict['arg2'] = commandElement
+            if '(' in line:
+                line = line.split('(')[0] + ' ( ' + line.split('(')[1]
 
-        self.parsedVMLines.append(parsedVMDict)
+            if ')' in line:
+                line = line.split(')')[0] + ' ) ' + line.split(')')[1]
+
+            if '[' in line:
+                line = line.split('[')[0] + ' [ ' + line.split('[')[1]
+
+            if ']' in line:
+                line = line.split(']')[0] + ' ] ' + line.split(']')[1]
+
+            if ';' in line:
+                line = line.split(';')[0] + ' ;'
+
+            if '<' in line:
+                line = line.replace('<', '&lt;')
+
+            if '>' in line:
+                line = line.replace('<', '&gt;')
+
+            if '"' in line:
+                splitedLine = (line.split('"'))
+                self._tokensList.extend(splitedLine[0].split())
+                self._tokensList.append(splitedLine[1])
+                self._tokensList.extend(splitedLine[2].split())
+
+            else:
+                self._tokensList.extend(line.split())
+
+        print(self._tokensList)
 
     def _has_more_tokens(self):
-        if len(self._commandsList) > 0:
+        if len(self._tokensList) > 0:
             return True
         else:
             return False
 
     def _advance(self):
-        self._currentCommand = self._commandsList.pop(0)
+        self._currentToken = self._tokensList.pop(0)
+
+    def _tokenType(self):
+
+        keywordList = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean',
+                       'void',
+                       'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
+
+        symbolList = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '&lt;', '&gt;', '=', '~']
+
+        if self._currentToken in keywordList:
+            tokenType = 'keyword'
+
+        elif self._currentToken in symbolList:
+            tokenType = 'symbol'
+
+        elif not self._currentToken[0].isdigit() and not ' ' in self._currentToken:
+            tokenType = 'identifier'
+
+        elif self._currentToken.isdigit() and int(self._currentToken) in range(0, 32767):
+            tokenType = 'integerConstant'
+
+        elif not '\n' in self._currentToken and not '"' in self._currentToken:
+            tokenType = 'stringConstant'
+
+        else:
+            print('invalid token!')
+
+        tXMLLine = '<' + tokenType + '> ' + self._currentToken + ' </' + tokenType + '>' + '\n'
+
+        self._XMLTokensList.append(tXMLLine)
+        return
+
+    def _writeXML(self, filePath):
+
+        # outFilePath ex: ArrayTest/Main.jack
+        outDirPath = filePath.split('/')[0]
+        outFileName = filePath.split('/')[1].split('.')[0]
+        outFilePath = outDirPath + '/' + 'My' + outFileName + 'T.xml'
+
+        self._XMLTokensList.append('</tokens>')
+
+        with open(outFilePath, 'w') as f:
+            for line in self._XMLTokensList:
+                if line:
+                    print(line)
+                    f.write(line)
