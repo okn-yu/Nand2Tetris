695d84266a6d044539c34a5205604159a9373ce9
okn-yu <haruka.koishikawa@gmail.com>
2018-09-23 17:53:37 +0900

    updated.

--- 06/hackassembler/assembler.py
+++ 06/hackassembler/assembler.py
@@ -1,21 +1,27 @@
-from Code import Code
-from Parser import Parser
-from SymbolTable import SymbolTable
 import sys
+
+from hackassembler import code
+from hackassembler import parser
+from hackassembler import symboltable
+
 
 if __name__ == "__main__":
 
+    # usage ex: python -m hackassembler.assembler max/Max.asm
+    # filePath ex: max/Max.asm
+    # outDirName ex: max
+    # outFileName ex: MyMax.hack
+    # outFilePath ex: max/MyMax.hack
+
     argvs = sys.argv
-    # filePath example: max/Max.asm
     filePath = argvs[1]
     outDirName = filePath.split('/')[0]
-    outFileName = 'My' + filePath.split('/')[1].split('.')[0]
-    # outFilePath example: max/MyMax.hack
-    outFilePath= outDirName + '/' + outFileName + '.hack'
+    outFileName = 'My' + filePath.split('/')[1].split('.')[0] + '.hack'
+    outFilePath= outDirName + '/' + outFileName
 
-    parser = Parser(filePath)
-    symbolTable = SymbolTable(parser.parsedAsmLines)
-    asmCode = Code(parser.parsedAsmLines, symbolTable.symbolDict)
+    parser = parser.Parser(filePath)
+    symbolTable = symboltable.SymbolTable(parser.parsedAsmLines)
+    asmCode = code.Code(parser.parsedAsmLines, symbolTable.symbolDict)
 
     with open(outFilePath, 'w') as f:
         for line in asmCode.binaryList:
