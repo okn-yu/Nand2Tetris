968c401403a26c1390e62ab0238a4fb2641a41da
okn-yu <haruka.koishikawa@gmail.com>
2018-09-27 23:55:04 +0900

    updated.

--- 07/hackcompiler/vm_translator.py
+++ 07/hackcompiler/vm_translator.py
@@ -1,6 +1,5 @@
 # Usage:
-# python -m hackcompiler.VMTranslator MemoryAccess/BasicTest/BasicTest.vm or
-# PYTHONPATH=. python hackcompiler/vm_translator.py MemoryAccess/BasicTest/BasicTest.vm
+# python -m hackcompiler.VMTranslator MemoryAccess/BasicTest/BasicTest.vm
 
 import sys
 
@@ -9,7 +8,6 @@ from hackcompiler import code_writer
 
 if __name__ == "__main__":
     argvs = sys.argv
-    # filePath example: StackArithmetic/SimpleAdd/SimpleAdd.vm
     filePath = argvs[1]
 
     parser = parser.Parser(filePath)
